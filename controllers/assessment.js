0a4f604db5f3b0a188d39eb2a31e8897d4e4bcf7
Glitch (james-obrien-gym-app) <none>
2018-08-14 17:36:55 +0000

    🌷🎮 Checkpoint ./controllers/assessment.js:1221218/148 ./views/dashboard.hbs:1221218/12 ./views/partials/status.hbs:1221218/88

--- controllers/assessment.js
+++ controllers/assessment.js
@@ -41,11 +41,13 @@ const assessment = {
   },
   
   calculateBmi(request, response) {
-   var weight = request.params.weight;
-  const
-    
+    var weight = request.params.weight;
+    var height = request.params.height ;
+    if(weight > 0 && height > 0){	
+
+    var finalBmi = weight/(height/100*height/100)
+  }
   },
-  
 };
 
  
