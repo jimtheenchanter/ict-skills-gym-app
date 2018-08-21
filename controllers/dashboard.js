1830b956d96802c80566c6b2b926beb5761e607b
Glitch (james-obrien-gym-app) <none>
2018-08-21 14:02:53 +0000

    🚵🛥 Checkpoint ./controllers/dashboard.js:1221218/153

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -17,6 +17,7 @@ const dashboard = {
     const viewData = {
       title: 'Dashboard',
       assessments: assessmentStore.getMemberAssessments(loggedInMember.id),
+      firstName: loggedInMember.firstName,
       bmiCategory: bmiCalc.determineCategory(bmiCalc.calculateBmi(loggedInMember,loggedInMember.startingweight)),
       bmi: bmiCalc.bmiValue,
       goals: goalStore.getMemberGoals(loggedInMember.id),
@@ -49,7 +50,7 @@ const dashboard = {
       chest: request.body.chest,
       waist: request.body.waist,
       hips: request.body.hips,
-      bmi:  bmiCalc.bmiValue
+      bmi:  bmiCalc.calculateBmi
     };
     logger.debug('Creating a new Assessment', newAssessment);
     assessmentStore.addAssessment(newAssessment);
