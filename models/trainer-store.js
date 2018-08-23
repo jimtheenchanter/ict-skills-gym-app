235ec9b6c6f51803da608719323cf502ea786e2d
Glitch (james-obrien-gym-app) <none>
2018-08-23 21:24:01 +0100

    Updated trainer accounts, assessment controller, tidied up and commented code

---
+++ models/trainer-store.js
@@ -0,0 +1,36 @@
+//model to
+
+'use strict';
+
+const _ = require('lodash');
+const JsonStore = require('./json-store');
+
+const trainerStore = {
+
+    store: new JsonStore('./models/trainer-store.json', { trainers: [] }),
+    collection: 'trainers',
+
+    getAllTrainers() {
+        return this.store.findAll(this.collection);
+    },
+
+    addTrainer(trainer) {
+        this.store.add(this.collection, trainer);
+    },
+
+    getTrainerById(id) {
+        return this.store.findOneBy(this.collection, { id: id });
+    },
+
+    getTrainerByEmail(email) {
+        return this.store.findOneBy(this.collection, { email: email });
+    },
+
+    getTrainerByPassword(password) {
+        return this.storefindOneBy(this.collection, { password: password });
+    },
+
+
+};
+
+module.exports = trainerStore;
